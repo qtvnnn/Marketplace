604efd027e58bca48bf7d033771fc2fd49bd3a24
CuongNguyen260601 <cuongnt2001.06.26@gmail.com>
2021-08-17 08:45:25 +0700

    cuong day code hoan thien

--- src/Writer_Contract/functionWriteMarketplace.js
+++ src/Writer_Contract/functionWriteMarketplace.js
@@ -5,22 +5,25 @@ export const ChuyenDiaChiKho = (contract,account,loaiKho,diaChiKho)=>{
 export const HuyBanHang = (contract,account,mahopdong)=>{
     return  contract.methods.HuyBanHang(mahopdong).send({from:account});
 }
+// Không cần gọi hàm gì trước
 
 export const ThamGiaDauGia = (contract, account, maphien, tiendaugia)=>{
     return contract.methods.DauGia(maphien,tiendaugia).send({from: account});
 }
-
+// approve tiền cho contract trước
 export const KetThucPhienDauGia = (contract,account,maphien)=>{
     return contract.methods.KetThucPhienDauGia(maphien).send({from:account});
 }
+// không cần gọi
 
 export const MuaHang = (contract,account,mahopdong,tienguivao)=>{
     return contract.methods.MuaHang(mahopdong,tienguivao).send({from: account});
 }
-
+// approve tien
 export const MuaLuonPhienDauGia = (contract,account,maphien,tienmualuon)=>{
     return contract.methods.MuaLuonPhienDauGia(maphien,tienmualuon).send({from: account});
 }
+//approve tien
 
 export const TaoHopDongMuaBan = (contract,account,tokenId,tienHang)=>{
     return contract.methods.TaoHopDongMuaBan(tokenId,tienHang).send({from: account});
