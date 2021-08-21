1cda8bbd0fa0116aa8f4bcb8cf05d7b95dfcf8a4
nangnn <qtvnnn@gmail.com>
2021-08-21 21:05:21 +0700

    ghep code

--- src/Function/getWeb3.js
+++ src/Function/getWeb3.js
@@ -1,40 +1,44 @@
 import Web3 from "web3";
 
-
-export default async()=>{
-
+export default async () => {
   let web3;
 
-  if(window.ethereum && window.ethereum.isMetaMask){
+  if (window.ethereum && window.ethereum.isMetaMask) {
     const network = window.ethereum.networkVersion;
-    switch(network){
-      case '1': {
-        alert('Please, connect network rinkeby !');
-        window.ethereum.on('disconnect');
+    switch (network) {
+      case "1": {
+        alert("Please, connect network rinkeby !");
+        window.ethereum.on("disconnect");
         return null;
       }
-      case '2': {
-        alert('Please, connect network rinkeby !');
-        window.ethereum.on('disconnect');
+      case "2": {
+        alert("Please, connect network rinkeby !");
+        window.ethereum.on("disconnect");
         return null;
       }
-      case '3': {
-        alert('Please, connect network rinkeby !');
-        window.ethereum.on('disconnect');
+      case "3": {
+        alert("Please, connect network rinkeby !");
+        window.ethereum.on("disconnect");
         return null;
       }
-      case '4': {
-        if ( window.ethereum !== "undefined" && typeof window.ethereum != "undefined") {
+      case "4": {
+        if (
+          window.ethereum !== "undefined" &&
+          typeof window.ethereum != "undefined"
+        ) {
           try {
             web3 = new Web3(window.ethereum);
-            await window.ethereum.request({ method: 'eth_requestAccounts' });;
+            await window.ethereum.request({ method: "eth_requestAccounts" });
             alert("Connect success !");
             return web3;
           } catch (error) {
             alert("Please, allow Metamask access to your account !");
             return null;
           }
-        } else if ( typeof window !== "undefined" && typeof window.web3 != "undefined") {
+        } else if (
+          typeof window !== "undefined" &&
+          typeof window.web3 != "undefined"
+        ) {
           web3 = new Web3(window.web3.currentProvider);
           alert("Connect success !");
           return web3;
@@ -45,17 +49,17 @@ export default async()=>{
       }
       case "42": {
         alert("Please, connect network rinkeby !");
-        window.ethereum.on('disconnect');
+        window.ethereum.on("disconnect");
         return null;
       }
-      default:{
+      default: {
         alert("Please, connect network rinkeby !");
-        window.ethereum.on('disconnect');
+        window.ethereum.on("disconnect");
         return null;
       }
     }
-  }else{
-    window.alert('Please, you need install Metamask !');
+  } else {
+    window.alert("Please, you need install Metamask !");
     return null;
   }
-}
+};
